016a8e3e20c27cafed8f6b6da07a9bb0da1cce64
Ccezzionale <tringali0511@gmail.com>
2025-06-10 15:11:04 +0200

    Update rose.js

--- rose.js
+++ rose.js
@@ -1,297 +1,704 @@
 const rose = {
-  "wildboys78": {
-    "nome": "Wildboys78",
-    "logo": "img/wildboys78.png",
-    "giocatori": [
-      {
-        "nome": "Provedel",
-        "ruolo": "P",
-        "tag": "FP"
-      },
-      {
-        "nome": "Gatti",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Carlos Augusto",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Bonaventura",
-        "ruolo": "C",
-        "tag": "FP"
-      },
-      {
-        "nome": "Gudmundsson",
-        "ruolo": "W",
-        "tag": "FP"
-      },
-      {
-        "nome": "Tchaouna",
-        "ruolo": "A",
-        "tag": "U21"
-      },
-      {
-        "nome": "Bonny",
-        "ruolo": "A",
-        "tag": "FP"
-      }
-    ]
-  },
-  "celticred": {
-    "nome": "CelticRed",
-    "logo": "img/celticred.png",
-    "giocatori": [
-      {
-        "nome": "Meret",
-        "ruolo": "P",
-        "tag": "FP"
-      },
-      {
-        "nome": "Scalvini",
-        "ruolo": "D",
-        "tag": "U21"
-      },
-      {
-        "nome": "Olivera",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Anguissa",
-        "ruolo": "C",
-        "tag": "FP"
-      },
-      {
-        "nome": "Dybala",
-        "ruolo": "A",
-        "tag": "FP"
-      },
-      {
-        "nome": "Soul\u00e9",
-        "ruolo": "W",
-        "tag": "FP"
-      },
-      {
-        "nome": "Retegui",
-        "ruolo": "A",
-        "tag": "FP"
-      }
-    ]
-  },
-  "margotvfc": {
-    "nome": "MargotVFC",
-    "logo": "img/margotvfc.png",
-    "giocatori": [
-      {
-        "nome": "Milinkovic-Savic",
-        "ruolo": "P",
-        "tag": "FP"
-      },
-      {
-        "nome": "Bremer",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Buongiorno",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Loftus-Cheek",
-        "ruolo": "C",
-        "tag": "FP"
-      },
-      {
-        "nome": "Zirkzee",
-        "ruolo": "A",
-        "tag": "FP"
-      }
-    ]
-  },
-  "asmonaco": {
-    "nome": "AS Monaco",
-    "logo": "img/asmonaco.png",
-    "giocatori": [
-      {
-        "nome": "Skorupski",
-        "ruolo": "P",
-        "tag": "FP"
-      },
-      {
-        "nome": "Calafiori",
-        "ruolo": "D",
-        "tag": "U21"
-      },
-      {
-        "nome": "Theo Hernandez",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Fagioli",
-        "ruolo": "C",
-        "tag": "U21"
-      },
-      {
-        "nome": "Berardi",
-        "ruolo": "A",
-        "tag": "FP"
-      },
-      {
-        "nome": "Osimhen",
-        "ruolo": "A",
-        "tag": "FP"
-      }
-    ]
-  },
-  "teamfortress": {
-    "nome": "TeamFortress",
-    "logo": "img/teamfortress.png",
-    "giocatori": [
-      {
-        "nome": "Consigli",
-        "ruolo": "P",
-        "tag": "FP"
-      },
-      {
-        "nome": "Djidji",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Mazzocchi",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Maleh",
-        "ruolo": "C",
-        "tag": "FP"
-      },
-      {
-        "nome": "Laurient\u00e9",
-        "ruolo": "W",
-        "tag": "FP"
-      },
-      {
-        "nome": "Jovic",
-        "ruolo": "A",
-        "tag": "FP"
-      }
-    ]
-  },
-  "danimarca": {
-    "nome": "Danimarca",
-    "logo": "img/danimarca.png",
-    "giocatori": [
-      {
-        "nome": "Montip\u00f2",
-        "ruolo": "P",
-        "tag": "FP"
-      },
-      {
-        "nome": "Bijol",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Birindelli",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Lobotka",
-        "ruolo": "C",
-        "tag": "FP"
-      },
-      {
-        "nome": "Orsolini",
-        "ruolo": "W",
-        "tag": "FP"
-      },
-      {
-        "nome": "Colombo",
-        "ruolo": "A",
-        "tag": "U21"
-      }
-    ]
-  },
-  "boca": {
-    "nome": "Boca Juniors",
-    "logo": "img/boca.png",
-    "giocatori": [
-      {
-        "nome": "Carnesecchi",
-        "ruolo": "P",
-        "tag": "U21"
-      },
-      {
-        "nome": "Milenkovic",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Spinazzola",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Ricci",
-        "ruolo": "C",
-        "tag": "U21"
-      },
-      {
-        "nome": "Lookman",
-        "ruolo": "W",
-        "tag": "FP"
-      },
-      {
-        "nome": "Zapata",
-        "ruolo": "A",
-        "tag": "FP"
-      }
-    ]
-  },
-  "sambuca": {
-    "nome": "Sambuca",
-    "logo": "img/sambuca.png",
-    "giocatori": [
-      {
-        "nome": "Sommer",
-        "ruolo": "P",
-        "tag": "FP"
-      },
-      {
-        "nome": "Demiral",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Dumfries",
-        "ruolo": "D",
-        "tag": "FP"
-      },
-      {
-        "nome": "Bennacer",
-        "ruolo": "C",
-        "tag": "FP"
-      },
-      {
-        "nome": "El Shaarawy",
-        "ruolo": "W",
-        "tag": "FP"
-      },
-      {
-        "nome": "Immobile",
-        "ruolo": "A",
-        "tag": "FP"
-      }
-    ]
-  }
+  "Team Bartowski": [
+    {
+      "nome": "Reina",
+      "status": "fp"
+    },
+    {
+      "nome": "Vigorito",
+      "status": "fp"
+    },
+    {
+      "nome": "Butez",
+      "status": "fp"
+    },
+    {
+      "nome": "Kolasinac",
+      "status": "fp"
+    },
+    {
+      "nome": "Pavard",
+      "status": "fp"
+    },
+    {
+      "nome": "Maripan",
+      "status": "fp"
+    },
+    {
+      "nome": "Mkhitaryan",
+      "status": "fp"
+    },
+    {
+      "nome": "Lobotka",
+      "status": "fp"
+    },
+    {
+      "nome": "Elmas",
+      "status": "fp"
+    },
+    {
+      "nome": "McKennie",
+      "status": "fp"
+    },
+    {
+      "nome": "Miretti",
+      "status": "fp"
+    },
+    {
+      "nome": "Makoumbou",
+      "status": "fp"
+    },
+    {
+      "nome": "Baldanzi",
+      "status": "u21"
+    }
+  ],
+  "Rubin Kebab": [
+    {
+      "nome": "Reina",
+      "status": "fp"
+    },
+    {
+      "nome": "Vigorito",
+      "status": "fp"
+    },
+    {
+      "nome": "Butez",
+      "status": "fp"
+    },
+    {
+      "nome": "Kristensen T.",
+      "status": "fp"
+    },
+    {
+      "nome": "Zielinski",
+      "status": "fp"
+    },
+    {
+      "nome": "Freuler",
+      "status": "fp"
+    },
+    {
+      "nome": "Ilic",
+      "status": "fp"
+    },
+    {
+      "nome": "Da Cunha",
+      "status": "fp"
+    },
+    {
+      "nome": "Frendrup",
+      "status": "fp"
+    },
+    {
+      "nome": "Oristanio",
+      "status": "fp"
+    },
+    {
+      "nome": "Folorunsho",
+      "status": "fp"
+    },
+    {
+      "nome": "De Ketelaere",
+      "status": "fp"
+    }
+  ],
+  "wildboys78": [
+    {
+      "nome": "Provedel",
+      "status": "fp"
+    },
+    {
+      "nome": "Furlanetto",
+      "status": "fp"
+    },
+    {
+      "nome": "Mandas",
+      "status": "fp"
+    },
+    {
+      "nome": "Acerbi",
+      "status": "fp"
+    },
+    {
+      "nome": "Walker",
+      "status": "fp"
+    },
+    {
+      "nome": "Zappa",
+      "status": "fp"
+    },
+    {
+      "nome": "Thiaw",
+      "status": "fp"
+    },
+    {
+      "nome": "Freuler",
+      "status": "fp"
+    },
+    {
+      "nome": "Guendouzi",
+      "status": "fp"
+    },
+    {
+      "nome": "Ricci S.",
+      "status": "fp"
+    },
+    {
+      "nome": "Ramadani",
+      "status": "fp"
+    },
+    {
+      "nome": "Ekkelenkamp",
+      "status": "fp"
+    },
+    {
+      "nome": "Isaksen",
+      "status": "fp"
+    },
+    {
+      "nome": "Bonny",
+      "status": "fp"
+    },
+    {
+      "nome": "Tchaouna",
+      "status": "u21"
+    }
+  ],
+  "Desperados": [
+    {
+      "nome": "Brancolini",
+      "status": "fp"
+    },
+    {
+      "nome": "Vasquez D.",
+      "status": "fp"
+    },
+    {
+      "nome": "Seghetti",
+      "status": "fp"
+    },
+    {
+      "nome": "Pavard",
+      "status": "fp"
+    },
+    {
+      "nome": "Sverko",
+      "status": "fp"
+    },
+    {
+      "nome": "Douglas Luiz",
+      "status": "fp"
+    },
+    {
+      "nome": "Messias",
+      "status": "fp"
+    },
+    {
+      "nome": "Samardzic",
+      "status": "fp"
+    },
+    {
+      "nome": "Sohm",
+      "status": "fp"
+    },
+    {
+      "nome": "Ricci S.",
+      "status": "fp"
+    },
+    {
+      "nome": "Busio",
+      "status": "fp"
+    },
+    {
+      "nome": "Gonzalez N.",
+      "status": "fp"
+    },
+    {
+      "nome": "Sarr A.",
+      "status": "fp"
+    },
+    {
+      "nome": "Beltran L.",
+      "status": "fp"
+    },
+    {
+      "nome": "Fazzini",
+      "status": "u21"
+    }
+  ],
+  "MinneSota Snakes": [
+    {
+      "nome": "Padelli",
+      "status": "fp"
+    },
+    {
+      "nome": "Okoye",
+      "status": "fp"
+    },
+    {
+      "nome": "Sava",
+      "status": "fp"
+    },
+    {
+      "nome": "Acerbi",
+      "status": "fp"
+    },
+    {
+      "nome": "Viti",
+      "status": "fp"
+    },
+    {
+      "nome": "Coco",
+      "status": "fp"
+    },
+    {
+      "nome": "Maldini",
+      "status": "fp"
+    },
+    {
+      "nome": "Koopmeiners",
+      "status": "fp"
+    },
+    {
+      "nome": "Frendrup",
+      "status": "fp"
+    },
+    {
+      "nome": "Ferguson",
+      "status": "fp"
+    },
+    {
+      "nome": "Colpani",
+      "status": "fp"
+    },
+    {
+      "nome": "Dele-Bashiru",
+      "status": "fp"
+    },
+    {
+      "nome": "Joao Felix",
+      "status": "fp"
+    },
+    {
+      "nome": "Fabbian",
+      "status": "u21"
+    }
+  ],
+  "PokerMantra": [
+    {
+      "nome": "Padelli",
+      "status": "fp"
+    },
+    {
+      "nome": "Okoye",
+      "status": "fp"
+    },
+    {
+      "nome": "Sava",
+      "status": "fp"
+    },
+    {
+      "nome": "Gabbia",
+      "status": "fp"
+    },
+    {
+      "nome": "Ruggeri",
+      "status": "fp"
+    },
+    {
+      "nome": "Baschirotto",
+      "status": "fp"
+    },
+    {
+      "nome": "Rovella",
+      "status": "fp"
+    },
+    {
+      "nome": "Pobega",
+      "status": "fp"
+    },
+    {
+      "nome": "Zerbin",
+      "status": "fp"
+    },
+    {
+      "nome": "Arnautovic",
+      "status": "fp"
+    },
+    {
+      "nome": "Cancellieri",
+      "status": "fp"
+    },
+    {
+      "nome": "Yildiz",
+      "status": "u21"
+    }
+  ],
+  "Bayern Christiansen": [
+    {
+      "nome": "Maripan",
+      "status": "fp"
+    },
+    {
+      "nome": "Ikone'",
+      "status": "fp"
+    },
+    {
+      "nome": "McKennie",
+      "status": "fp"
+    },
+    {
+      "nome": "Koopmeiners",
+      "status": "fp"
+    },
+    {
+      "nome": "Zerbin",
+      "status": "fp"
+    },
+    {
+      "nome": "Castro S.",
+      "status": "u21"
+    }
+  ],
+  "Minnesode Timberland": [
+    {
+      "nome": "Pavlovic",
+      "status": "fp"
+    },
+    {
+      "nome": "Olivera",
+      "status": "fp"
+    },
+    {
+      "nome": "Circati",
+      "status": "fp"
+    },
+    {
+      "nome": "Viola",
+      "status": "fp"
+    },
+    {
+      "nome": "Locatelli",
+      "status": "fp"
+    },
+    {
+      "nome": "Oristanio",
+      "status": "fp"
+    },
+    {
+      "nome": "Arnautovic",
+      "status": "fp"
+    },
+    {
+      "nome": "De Ketelaere",
+      "status": "fp"
+    },
+    {
+      "nome": "Scalvini",
+      "status": "u21"
+    }
+  ],
+  "Sharknado 04": [
+    {
+      "nome": "Marcone",
+      "status": "fp"
+    },
+    {
+      "nome": "Suzuki",
+      "status": "fp"
+    },
+    {
+      "nome": "Corvi",
+      "status": "fp"
+    },
+    {
+      "nome": "Romagnoli",
+      "status": "fp"
+    },
+    {
+      "nome": "Walker",
+      "status": "fp"
+    },
+    {
+      "nome": "Kolasinac",
+      "status": "fp"
+    },
+    {
+      "nome": "Lucumi'",
+      "status": "fp"
+    },
+    {
+      "nome": "Beukema",
+      "status": "fp"
+    },
+    {
+      "nome": "Raspadori",
+      "status": "fp"
+    },
+    {
+      "nome": "Kolo Muani",
+      "status": "fp"
+    },
+    {
+      "nome": "Fabbian",
+      "status": "u21"
+    }
+  ],
+  "Real Mimmo": [
+    {
+      "nome": "Lucumi'",
+      "status": "fp"
+    },
+    {
+      "nome": "Gaspar K.",
+      "status": "fp"
+    },
+    {
+      "nome": "Coco",
+      "status": "fp"
+    },
+    {
+      "nome": "Paredes",
+      "status": "fp"
+    },
+    {
+      "nome": "Hernani",
+      "status": "fp"
+    },
+    {
+      "nome": "Da Cunha",
+      "status": "fp"
+    },
+    {
+      "nome": "Miretti",
+      "status": "fp"
+    },
+    {
+      "nome": "Lovric",
+      "status": "fp"
+    },
+    {
+      "nome": "Ferguson",
+      "status": "fp"
+    },
+    {
+      "nome": "Jovic",
+      "status": "fp"
+    },
+    {
+      "nome": "Sarr A.",
+      "status": "fp"
+    },
+    {
+      "nome": "Bonny",
+      "status": "u21"
+    }
+  ],
+  "Giody": [
+    {
+      "nome": "Biraghi",
+      "status": "fp"
+    },
+    {
+      "nome": "Pavlovic",
+      "status": "fp"
+    },
+    {
+      "nome": "Hien",
+      "status": "fp"
+    },
+    {
+      "nome": "Pellegrini Lo.",
+      "status": "fp"
+    },
+    {
+      "nome": "Pasalic",
+      "status": "fp"
+    },
+    {
+      "nome": "Fagioli",
+      "status": "fp"
+    },
+    {
+      "nome": "Makoumbou",
+      "status": "fp"
+    },
+    {
+      "nome": "Goglichidze",
+      "status": "u21"
+    }
+  ],
+  "Ibla": [
+    {
+      "nome": "Provedel",
+      "status": "fp"
+    },
+    {
+      "nome": "Furlanetto",
+      "status": "fp"
+    },
+    {
+      "nome": "Mandas",
+      "status": "fp"
+    },
+    {
+      "nome": "Djimsiti",
+      "status": "fp"
+    },
+    {
+      "nome": "Mina",
+      "status": "fp"
+    },
+    {
+      "nome": "Gabbia",
+      "status": "fp"
+    },
+    {
+      "nome": "Ruggeri",
+      "status": "fp"
+    },
+    {
+      "nome": "Jimenez A.",
+      "status": "fp"
+    },
+    {
+      "nome": "Savona",
+      "status": "fp"
+    },
+    {
+      "nome": "Alberto Costa",
+      "status": "fp"
+    },
+    {
+      "nome": "Locatelli",
+      "status": "fp"
+    },
+    {
+      "nome": "Fagioli",
+      "status": "fp"
+    },
+    {
+      "nome": "Casadei",
+      "status": "fp"
+    },
+    {
+      "nome": "Dele-Bashiru",
+      "status": "fp"
+    },
+    {
+      "nome": "Gonzalez N.",
+      "status": "fp"
+    },
+    {
+      "nome": "Krstovic",
+      "status": "fp"
+    },
+    {
+      "nome": "Soule'",
+      "status": "u21"
+    }
+  ],
+  "Pandinicoccolosini": [
+    {
+      "nome": "Masina",
+      "status": "fp"
+    },
+    {
+      "nome": "Izzo",
+      "status": "fp"
+    },
+    {
+      "nome": "Romagnoli",
+      "status": "fp"
+    },
+    {
+      "nome": "Sabelli",
+      "status": "fp"
+    },
+    {
+      "nome": "Hummels",
+      "status": "fp"
+    },
+    {
+      "nome": "Cataldi",
+      "status": "fp"
+    },
+    {
+      "nome": "Pasalic",
+      "status": "fp"
+    },
+    {
+      "nome": "Rovella",
+      "status": "fp"
+    },
+    {
+      "nome": "Samardzic",
+      "status": "fp"
+    },
+    {
+      "nome": "Tete Morente",
+      "status": "fp"
+    },
+    {
+      "nome": "Liberali",
+      "status": "fp"
+    },
+    {
+      "nome": "Ekkelenkamp",
+      "status": "fp"
+    },
+    {
+      "nome": "Fadera",
+      "status": "fp"
+    },
+    {
+      "nome": "Gourna-Douath",
+      "status": "fp"
+    },
+    {
+      "nome": "Joao Felix",
+      "status": "fp"
+    },
+    {
+      "nome": "Yildiz",
+      "status": "u21"
+    }
+  ],
+  "Giulay": [
+    {
+      "nome": "Biraghi",
+      "status": "fp"
+    },
+    {
+      "nome": "Pavlovic",
+      "status": "fp"
+    },
+    {
+      "nome": "Hien",
+      "status": "fp"
+    },
+    {
+      "nome": "Pellegrini Lo.",
+      "status": "fp"
+    },
+    {
+      "nome": "Pasalic",
+      "status": "fp"
+    },
+    {
+      "nome": "Fagioli",
+      "status": "fp"
+    },
+    {
+      "nome": "Makoumbou",
+      "status": "fp"
+    },
+    {
+      "nome": "Goglichidze",
+      "status": "u21"
+    }
+  ]
 };
 
 window.rose = rose;
