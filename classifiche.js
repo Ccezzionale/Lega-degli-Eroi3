0bad815cb188946b778c2139dba4db2ca58248be
Ccezzionale <tringali0511@gmail.com>
2025-06-10 16:57:56 +0200

    Add files via upload

--- classifiche.js
+++ classifiche.js
@@ -1,18 +1,1 @@
-const classifiche = {
-  roundrobin: [
-    ["Pos", "Squadra", "Punti"],
-    ["1", "Real Mimmo", "78"],
-    ["2", "PokerMantra", "75"],
-    ["3", "Desperados", "72"]
-  ],
-  conference: [
-    ["Pos", "Squadra", "Punti"],
-    ["1", "Team Bartowski", "40"],
-    ["2", "Giulay", "38"]
-  ],
-  championship: [
-    ["Pos", "Squadra", "Punti"],
-    ["1", "Ibla", "60"],
-    ["2", "Rubin Kebab", "58"]
-  ]
-};
+const classifiche = {}; // Inserisci qui i dati reali
