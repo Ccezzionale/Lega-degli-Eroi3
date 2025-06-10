20e691dddb6bc7bf1b58df3f714a6484c6e1f5a7
Ccezzionale <tringali0511@gmail.com>
2025-06-10 15:38:49 +0200

    Update rose.js

--- rose.js
+++ rose.js
@@ -1,140 +1,759 @@
 const rose = {
-  "Team Bartowski": {
+"Team Bartowski": {
     "nome": "Team Bartowski",
     "logo": "img/team_bartowski.png",
     "giocatori": [
-      {
-        "nome": "Reina",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Vigorito",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Butez",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Kolasinac",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Pavard",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Maripan",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Mkhitaryan",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Lobotka",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Elmas",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "McKennie",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Miretti",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Makoumbou",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Baldanzi",
-        "ruolo": "-",
-        "tag": "U21"
-      }
-    ]
-  },
+    {
+      "nome": "Reina",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Vigorito",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Butez",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Kolasinac",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pavard",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Maripan",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Mkhitaryan",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Lobotka",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Elmas",
+      “tag”: "fp"
+    },
+    {
+      "nome": "McKennie",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Miretti",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Makoumbou",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Baldanzi",
+      “tag”: "u21"
+    }
+  ],
+},
   "Rubin Kebab": {
     "nome": "Rubin Kebab",
     "logo": "img/rubin_kebab.png",
     "giocatori": [
-      {
-        "nome": "Reina",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Vigorito",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Butez",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Kristensen T.",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Zielinski",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Freuler",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Ilic",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Da Cunha",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Frendrup",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Oristanio",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "Folorunsho",
-        "ruolo": "-",
-        "tag": "FP"
-      },
-      {
-        "nome": "De Ketelaere",
-        "ruolo": "-",
-        "tag": "FP"
-      }
-    ]
+    {
+      "nome": "Reina",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Vigorito",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Butez",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Kristensen T.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Zielinski",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Freuler",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ilic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Da Cunha",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Frendrup",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Oristanio",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Folorunsho",
+      “tag”: "fp"
+    },
+    {
+      "nome": "De Ketelaere",
+      “tag”: "fp"
+    }
+  ],
+},
+  " wildboys78": {
+    "nome": " wildboys78",
+    "logo": "img/ wildboys78.png",
+    "giocatori": [
+    {
+      "nome": "Provedel",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Furlanetto",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Mandas",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Acerbi",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Walker",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Zappa",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Thiaw",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Freuler",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Guendouzi",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ricci S.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ramadani",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ekkelenkamp",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Isaksen",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Bonny",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Tchaouna",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Desperados ": {
+    "nome": " Desperados ",
+    "logo": "img/ Desperados.png",
+    "giocatori": [
+    {
+      "nome": "Brancolini",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Vasquez D.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Seghetti",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pavard",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Sverko",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Douglas Luiz",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Messias",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Samardzic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Sohm",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ricci S.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Busio",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Gonzalez N.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Sarr A.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Beltran L.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Fazzini",
+      “tag”: "u21"
+    }
+  ],
+},
+  " MinneSota Snakes ": {
+    "nome": " MinneSota Snakes ",
+    "logo": "img/ MinneSota Snakes.png",
+    "giocatori": [
+    {
+      "nome": "Padelli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Okoye",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Sava",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Acerbi",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Viti",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Coco",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Maldini",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Koopmeiners",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Frendrup",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ferguson",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Colpani",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Dele-Bashiru",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Joao Felix",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Fabbian",
+      “tag”: "u21"
+    }
+  ],
+},
+  " PokerMantra ": {
+    "nome": " PokerMantra ",
+    "logo": "img/ PokerMantra.png",
+    "giocatori": [
+    {
+      "nome": "Padelli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Okoye",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Sava",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Gabbia",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ruggeri",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Baschirotto",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Rovella",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pobega",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Zerbin",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Arnautovic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Cancellieri",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Yildiz",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Bayern Christiansen ": {
+    "nome": " Bayern Christiansen ",
+    "logo": "img/ Bayern Christiansen.png",
+    "giocatori": [
+    {
+      "nome": "Maripan",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ikone'",
+      “tag”: "fp"
+    },
+    {
+      "nome": "McKennie",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Koopmeiners",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Zerbin",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Castro S.",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Minnesode Timberland ": {
+    "nome": " Minnesode Timberland ",
+    "logo": "img/ Minnesode Timberland.png",
+    "giocatori": [
+    {
+      "nome": "Pavlovic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Olivera",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Circati",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Viola",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Locatelli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Oristanio",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Arnautovic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "De Ketelaere",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Scalvini",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Sharknado 04": {
+    "nome": " Sharknado 04",
+    "logo": "img/ Sharknado 04.png",
+    "giocatori": [
+    {
+      "nome": "Marcone",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Suzuki",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Corvi",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Romagnoli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Walker",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Kolasinac",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Lucumi'",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Beukema",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Raspadori",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Kolo Muani",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Fabbian",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Real Mimmo ": {
+    "nome": " Real Mimmo ",
+    "logo": "img/ Real Mimmo.png",
+    "giocatori": [
+    {
+      "nome": "Lucumi'",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Gaspar K.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Coco",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Paredes",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Hernani",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Da Cunha",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Miretti",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Lovric",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ferguson",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Jovic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Sarr A.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Bonny",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Giody ": {
+    "nome": " Giody ",
+    "logo": "img/ Giody.png",
+    "giocatori": [
+    {
+      "nome": "Biraghi",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pavlovic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Hien",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pellegrini Lo.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pasalic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Fagioli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Makoumbou",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Goglichidze",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Ibla ": {
+    "nome": " Ibla ",
+    "logo": "img/ Ibla.png",
+    "giocatori": [
+    {
+      "nome": "Provedel",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Furlanetto",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Mandas",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Djimsiti",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Mina",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Gabbia",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ruggeri",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Jimenez A.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Savona",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Alberto Costa",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Locatelli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Fagioli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Casadei",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Dele-Bashiru",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Gonzalez N.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Krstovic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Soule'",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Pandinicoccolosini ": {
+    "nome": " Pandinicoccolosini ",
+    "logo": "img/ Pandinicoccolosini.png",
+    "giocatori": [
+    {
+      "nome": "Masina",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Izzo",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Romagnoli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Sabelli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Hummels",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Cataldi",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pasalic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Rovella",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Samardzic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Tete Morente",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Liberali",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Ekkelenkamp",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Fadera",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Gourna-Douath",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Joao Felix",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Yildiz",
+      “tag”: "u21"
+    }
+  ],
+},
+  " Giulay ": {
+    "nome": " Giulay ",
+    "logo": "img/ Giulay.png",
+    "giocatori": [
+    {
+      "nome": "Biraghi",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pavlovic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Hien",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pellegrini Lo.",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Pasalic",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Fagioli",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Makoumbou",
+      “tag”: "fp"
+    },
+    {
+      "nome": "Goglichidze",
+      “tag”: "u21"
+    }
+  ]
   }
 };
 
