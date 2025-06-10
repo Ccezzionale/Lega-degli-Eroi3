402d266d533ba213b9764509ac11f45047c734d9
Ccezzionale <tringali0511@gmail.com>
2025-06-10 14:44:10 +0200

    Add files via upload

--- rose.js
+++ rose.js
@@ -1,1 +1,41 @@
-const rose = {};
+const rose = {
+  "wildboys78": {
+    "nome": "Wildboys78",
+    "logo": "img/wildboys78.png",
+    "giocatori": [
+      {
+        "nome": "Provedel",
+        "ruolo": "P",
+        "tag": "FP"
+      },
+      {
+        "nome": "Tchaouna",
+        "ruolo": "A",
+        "tag": "U21"
+      },
+      {
+        "nome": "Bonny",
+        "ruolo": "A",
+        "tag": "FP"
+      }
+    ]
+  },
+  "celticred": {
+    "nome": "CelticRed",
+    "logo": "img/celticred.png",
+    "giocatori": [
+      {
+        "nome": "Meret",
+        "ruolo": "P",
+        "tag": "FP"
+      },
+      {
+        "nome": "Scalvini",
+        "ruolo": "D",
+        "tag": "U21"
+      }
+    ]
+  }
+};
+
+export default rose;
