6126631ceb881c255d8b1e02ce6b4f28274eee39
Ccezzionale <tringali0511@gmail.com>
2025-06-13 23:34:10 +0200

    Add files via upload

--- draft_script_unificato.js
+++ draft_script_unificato.js
@@ -24,7 +24,7 @@ function inviaPickAlFoglio(pick, fantaTeam, nome, ruolo, squadra, quotazione) {
   dati.append("ruolo", ruolo);
   dati.append("quotazione", quotazione);
 
-  fetch("https://script.google.com/macros/s/AKfycbwlbFaswGVUJeSDnlU-xpQYAnpk5C2kSB-R6Vz7jfXO63ijMkEzfpOjmM66QZwAvNlo/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbxFamdCRhlGfZ3j53yPUlGzE3dlrpxHvsJbXGht2D4fHJJ1HDybWoWg5Doin2d0BccF8Q/exec", {
     method: "POST",
     body: dati
   })
