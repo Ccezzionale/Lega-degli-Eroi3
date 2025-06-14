680a7da4e38e5f16cc1391fed9d72e60c80bfd7c
Ccezzionale <tringali0511@gmail.com>
2025-06-14 13:42:51 +0200

    Add files via upload

--- chiamate.js
+++ chiamate.js
@@ -1,8 +1,8 @@
 
 function caricaChiamate(conference) {
   const urls = {
-    champ: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK5ADcukTU83udU_Z9Zd9w66-2LGi8TlWJP_F5WfcaHQePIUpRBynnpbnxbkEGnrh44jMvvBo7Wzo3/pub?output=csv",
-    league: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGbT5qMBn_PHnAyGfR1IayL3BgrfBYENXZ1tMoBXjvoZxQAFVI5wRk7kY0M9sAXuJg0wVImKh0g_bB/pub?gid=1279168385&single=true&output=csv"
+    league: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK5ADcukTU83udU_Z9Zd9w66-2LGi8TlWJP_F5WfcaHQePIUpRBynnpbnxbkEGnrh44jMvvBo7Wzo3/pub?output=csv",
+    champ: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGbT5qMBn_PHnAyGfR1IayL3BgrfBYENXZ1tMoBXjvoZxQAFVI5wRk7kY0M9sAXuJg0wVImKh0g_bB/pub?gid=1279168385&single=true&output=csv"
   };
 
   const url = urls[conference];
@@ -15,16 +15,22 @@ function caricaChiamate(conference) {
       const intestazioni = righe[0];
       const dati = righe.slice(1);
 
+      if (dati.length === 1 && dati[0][0].startsWith("🔒")) {
+        document.getElementById('chiamate-container').innerHTML =
+          '<div class="avviso">' + dati[0][0] + '</div>';
+        return;
+      }
+
       let html = '<table><thead><tr>';
       for (let i = 0; i < intestazioni.length; i++) {
-        html += `<th>${intestazioni[i]}</th>`;
+        html += '<th>' + intestazioni[i] + '</th>';
       }
       html += '</tr></thead><tbody>';
 
       dati.forEach(riga => {
         if (riga.length >= 2 && riga[1].trim() !== '') {
           html += '<tr>';
-          riga.forEach(val => html += `<td>${val}</td>`);
+          riga.forEach(val => html += '<td>' + val + '</td>');
           html += '</tr>';
         }
       });
