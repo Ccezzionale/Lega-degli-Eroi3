06a6a1556a78922f039743c5cb65db9323da97fd
Ccezzionale <tringali0511@gmail.com>
2025-06-16 15:24:01 +0200

    Add files via upload

--- rose.js
+++ rose.js
@@ -1,1 +1,66 @@
-const rose = {};
+const rose = {
+  "Team Bartowski": {
+    "logo": "img/team_bartowski.png",
+    "giocatori": []
+  },
+  "Rubin Kebab": {
+    "logo": "img/rubin_kebab.png",
+    "giocatori": []
+  },
+  "Wildboys": {
+    "logo": "img/wildboys.png",
+    "giocatori": []
+  },
+  "Desperados": {
+    "logo": "img/desperados.png",
+    "giocatori": []
+  },
+  "Rafa Casablanca": {
+    "logo": "img/rafa_casablanca.png",
+    "giocatori": []
+  },
+  "PokerMantra": {
+    "logo": "img/pokermantra.png",
+    "giocatori": []
+  },
+  "Bayern Christiansen": {
+    "logo": "img/bayern_christiansen.png",
+    "giocatori": []
+  },
+  "Minnesode Timberland": {
+    "logo": "img/minnesode_timberland.png",
+    "giocatori": []
+  },
+  "Sharknado 04": {
+    "logo": "img/sharknado_04.png",
+    "giocatori": []
+  },
+  "Real Mimmo": {
+    "logo": "img/real_mimmo.png",
+    "giocatori": []
+  },
+  "Giody": {
+    "logo": "img/giody.png",
+    "giocatori": []
+  },
+  "Ibla": {
+    "logo": "img/ibla.png",
+    "giocatori": []
+  },
+  "Pandinicoccolosini": {
+    "logo": "img/pandinicoccolosini.png",
+    "giocatori": []
+  },
+  "Giulay": {
+    "logo": "img/giulay.png",
+    "giocatori": []
+  },
+  "MinneSota Snakes": {
+    "logo": "img/minnesota_snakes.png",
+    "giocatori": []
+  },
+  "Union Librino": {
+    "logo": "img/union_librino.png",
+    "giocatori": []
+  }
+};
