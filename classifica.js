ab60d101aad4885187b4e3dc5ffde642d422867e
Ccezzionale <tringali0511@gmail.com>
2025-06-19 22:53:13 +0200

    Update classifica.js

--- classifica.js
+++ classifica.js
@@ -125,8 +125,8 @@ td.appendChild(wrapper);
         logo.onerror = () => { logo.style.display = "none"; };
 
         const pos = colonne[0];
-        const punti = formattaNumero(colonne[colonne.length - 7]);
-        const puntiTot = formattaNumero(colonne[colonne.length - 6]);
+        const punti = formattaNumero(colonne[colonne.length - 3]);
+        const puntiTot = formattaNumero(colonne[colonne.length - 2]);
 
         const testo = document.createElement("span");
         testo.innerHTML = `<strong>${pos}. ${nomeSquadra}</strong><br><span style="font-weight:normal">PT. ${punti} / MP. ${puntiTot}</span>`;
