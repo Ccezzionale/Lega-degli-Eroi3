33dc1b8707efd25b224136cb232b813e5593c112
Ccezzionale <tringali0511@gmail.com>
2025-06-28 00:47:14 +0200

    Update arena.js

--- arena.js
+++ arena.js
@@ -1,21 +1,21 @@
 
 const squadre = [
   { nome: "Rubin Kebab", logo: "img/Rubinkebab.png", eliminata: true, ultimaEliminata: false },
-  { nome: "Bayern Christiansen", logo: "img/Bayern Christiansen.png", eliminata: false, ultimaEliminata: false },
+  { nome: "Bayern Christiansen", logo: "img/Bayern Christiansen.png", eliminata: true, ultimaEliminata: false },
   { nome: "Team Bartowski", logo: "img/Team Bartowski.png", eliminata: false, ultimaEliminata: true },
   { nome: "Real Mimmo", logo: "img/Real Mimmo.png", eliminata: true, ultimaEliminata: false },
-  { nome: "Union Librino", logo: "img/Union Librino.png", eliminata: false, ultimaEliminata: false },
+  { nome: "Union Librino", logo: "img/Union Librino.png", eliminata: true, ultimaEliminata: false },
   { nome: "Ibla", logo: "img/Ibla.png", eliminata: true, ultimaEliminata: false },
   { nome: "Rafa Casablanca", logo: "img/Rafa Casablanca.png", eliminata: true, ultimaEliminata: false },
-  { nome: "Giody", logo: "img/Giody.png", eliminata: false, ultimaEliminata: false },
-  { nome: "Desperados", logo: "img/Desperados.png", eliminata: false, ultimaEliminata: false },
-  { nome: "Wildboys 78", logo: "img/wildboys78.png", eliminata: false, ultimaEliminata: false },
-  { nome: "Pandinicoccolosini", logo: "img/Pandinicoccolosini.png", eliminata: false, ultimaEliminata: false },
+  { nome: "Giody", logo: "img/Giody.png", eliminata: true, ultimaEliminata: false },
+  { nome: "Desperados", logo: "img/Desperados.png", eliminata: true, ultimaEliminata: false },
+  { nome: "Wildboys 78", logo: "img/wildboys78.png", eliminata: true, ultimaEliminata: false },
+  { nome: "Pandinicoccolosini", logo: "img/Pandinicoccolosini.png", eliminata: true, ultimaEliminata: false },
   { nome: "Giulay", logo: "img/Giulay.png", eliminata: true, ultimaEliminata: false },
   { nome: "Pokermantra", logo: "img/PokerMantra.png", eliminata: true, ultimaEliminata: false },
   { nome: "Minnesode Timberland", logo: "img/Minnesode Timberland.png", eliminata: true, ultimaEliminata: false },
   { nome: "Minnesota Snakes", logo: "img/MinneSota Snakes.png", eliminata: true, ultimaEliminata: false },
-  { nome: "Sharknado04", logo: "img/Sharknado 04.png", eliminata: false, ultimaEliminata: false }
+  { nome: "Sharknado04", logo: "img/Sharknado 04.png", eliminata: true, ultimaEliminata: false }
 ];
 
 const center = document.getElementById("arena-center");
