1b41d4445f109ffa8850dcd284893edb7efb74f5
Ccezzionale <tringali0511@gmail.com>
2025-07-03 10:14:20 +0200

    Update league_draft.js

--- league_draft.js
+++ league_draft.js
@@ -1,1 +1,236 @@
-const leagueDraft = [{"Round":0,"Pick 1":"MinneSota Snakes","Pick 2":"Pandinicoccolosini","Pick 3":"Desperados","Pick 4":"Giulay","Pick 5":"Ibla","Pick 6":"Minnesode Timberland","Pick 7":"Bayern Christiansen","Pick 8":"Team Bartowski"},{"Round":1,"Pick 1":"Team Bartowski","Pick 2":"Bayern Christiansen","Pick 3":"Minnesode Timberland","Pick 4":"Ibla","Pick 5":"Giulay","Pick 6":"Desperados","Pick 7":"Pandinicoccolosini","Pick 8":"MinneSota Snakes"},{"Round":2,"Pick 1":"MinneSota Snakes","Pick 2":"Pandinicoccolosini","Pick 3":"Desperados","Pick 4":"Giulay","Pick 5":"Ibla","Pick 6":"Minnesode Timberland","Pick 7":"Bayern Christiansen","Pick 8":"Team Bartowski"},{"Round":3,"Pick 1":"Team Bartowski","Pick 2":"Bayern Christiansen","Pick 3":"Minnesode Timberland","Pick 4":"Ibla","Pick 5":"Giulay","Pick 6":"Desperados","Pick 7":"Pandinicoccolosini","Pick 8":"MinneSota Snakes"},{"Round":4,"Pick 1":"MinneSota Snakes","Pick 2":"Pandinicoccolosini","Pick 3":"Desperados","Pick 4":"Giulay","Pick 5":"Ibla","Pick 6":"Minnesode Timberland","Pick 7":"Bayern Christiansen","Pick 8":"Team Bartowski"},{"Round":5,"Pick 1":"Team Bartowski","Pick 2":"Bayern Christiansen","Pick 3":"Minnesode Timberland","Pick 4":"Ibla","Pick 5":"Giulay","Pick 6":"Desperados","Pick 7":"Pandinicoccolosini","Pick 8":"MinneSota Snakes"},{"Round":6,"Pick 1":"MinneSota Snakes","Pick 2":"Pandinicoccolosini","Pick 3":"Desperados","Pick 4":"Giulay","Pick 5":"Ibla","Pick 6":"Minnesode Timberland","Pick 7":"Bayern Christiansen","Pick 8":"Team Bartowski"},{"Round":7,"Pick 1":"Team Bartowski","Pick 2":"Bayern Christiansen","Pick 3":"Minnesode Timberland","Pick 4":"Ibla","Pick 5":"Giulay","Pick 6":"Desperados","Pick 7":"Pandinicoccolosini","Pick 8":"MinneSota Snakes"},{"Round":8,"Pick 1":"MinneSota Snakes","Pick 2":"Pandinicoccolosini","Pick 3":"Desperados","Pick 4":"Giulay","Pick 5":"Ibla","Pick 6":"Minnesode Timberland","Pick 7":"Bayern Christiansen","Pick 8":"Team Bartowski"},{"Round":9,"Pick 1":"Team Bartowski","Pick 2":"Bayern Christiansen","Pick 3":"Minnesode Timberland","Pick 4":"Ibla","Pick 5":"Giulay","Pick 6":"Desperados","Pick 7":"Pandinicoccolosini","Pick 8":"MinneSota Snakes"},{"Round":10,"Pick 1":"MinneSota Snakes","Pick 2":"Pandinicoccolosini","Pick 3":"Desperados","Pick 4":"Giulay","Pick 5":"Ibla","Pick 6":"Minnesode Timberland","Pick 7":"Bayern Christiansen","Pick 8":"Team Bartowski"},{"Round":11,"Pick 1":"Team Bartowski","Pick 2":"Bayern Christiansen","Pick 3":"Minnesode Timberland","Pick 4":"Ibla","Pick 5":"Giulay","Pick 6":"Desperados","Pick 7":"Pandinicoccolosini","Pick 8":"MinneSota Snakes"},{"Round":12,"Pick 1":"MinneSota Snakes","Pick 2":"Pandinicoccolosini","Pick 3":"Desperados","Pick 4":"Giulay","Pick 5":"Ibla","Pick 6":"Minnesode Timberland","Pick 7":"Bayern Christiansen","Pick 8":"Team Bartowski"},{"Round":13,"Pick 1":"Team Bartowski","Pick 2":"Bayern Christiansen","Pick 3":"Minnesode Timberland","Pick 4":"Ibla","Pick 5":"Giulay","Pick 6":"Desperados","Pick 7":"Pandinicoccolosini","Pick 8":"MinneSota Snakes"},{"Round":14,"Pick 1":"MinneSota Snakes","Pick 2":"Pandinicoccolosini","Pick 3":"Desperados","Pick 4":"Giulay","Pick 5":"Ibla","Pick 6":"Minnesode Timberland","Pick 7":"Bayern Christiansen","Pick 8":"Team Bartowski"}];
+const leagueDraft = [
+  {
+    "Round": 1,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 2,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  },
+  {
+    "Round": 3,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 4,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  },
+  {
+    "Round": 5,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 6,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  },
+  {
+    "Round": 7,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 8,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  },
+  {
+    "Round": 9,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 10,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  },
+  {
+    "Round": 11,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 12,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  },
+  {
+    "Round": 13,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 14,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  },
+  {
+    "Round": 15,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 16,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  },
+  {
+    "Round": 17,
+    "Picks": [
+      "MinneSota Snakes",
+      "Pandinicoccolosini",
+      "Desperados",
+      "Giulay",
+      "Minnesode Timberland",
+      "Ibla",
+      "Bayern Christiansen",
+      "Team Bartowski"
+    ]
+  },
+  {
+    "Round": 18,
+    "Picks": [
+      "Team Bartowski",
+      "Bayern Christiansen",
+      "Ibla",
+      "Minnesode Timberland",
+      "Giulay",
+      "Desperados",
+      "Pandinicoccolosini",
+      "MinneSota Snakes"
+    ]
+  }
+]
